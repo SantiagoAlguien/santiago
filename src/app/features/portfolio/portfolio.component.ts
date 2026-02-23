76c31453dc126cf65877151d7ef17d1bd389fd27
SantiagoAlguien <28.snll.05@gmail.com>
2026-02-22 20:41:46 -0500

    educacion

--- src/app/features/portfolio/portfolio.component.ts
+++ src/app/features/portfolio/portfolio.component.ts
@@ -4,13 +4,14 @@ import { ButtonModule } from 'primeng/button';
 import { ProfileComponent } from './components/profile/profile.component';
 import { ExperienceComponent } from './components/experience/experience.component';
 import { SkillsComponent } from './components/skills/skills.component';
+import { EducationComponent } from './components/education/education.component';
 
 
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [AvatarModule, ButtonModule, ProfileComponent, ExperienceComponent, SkillsComponent],
+  imports: [AvatarModule, ButtonModule, ProfileComponent, ExperienceComponent, SkillsComponent, EducationComponent],
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.scss']
 })
