d2ed2c9e72d98c2acfac59e0ad566cbe3fc036ab
SantiagoAlguien <28.snll.05@gmail.com>
2026-02-22 20:04:07 -0500

    experencia

--- src/app/features/portfolio/portfolio.component.ts
+++ src/app/features/portfolio/portfolio.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { AvatarModule } from 'primeng/avatar';
 import { ButtonModule } from 'primeng/button';
 import { ProfileComponent } from './components/profile/profile.component';
+import { ExperienceComponent } from './components/experience/experience.component';
 
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [AvatarModule, ButtonModule, ProfileComponent],
+  imports: [AvatarModule, ButtonModule, ProfileComponent, ExperienceComponent ],
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.scss']
 })
